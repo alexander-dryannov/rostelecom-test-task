1a8b1275857901a061333410dd00c4231f2debdc
Alexander Dryannov <l3x.d@yandex.ru>
2022-11-30 12:56:03 +0300

    Add validation form

--- frontend/src/components/MainForm.jsx
+++ frontend/src/components/MainForm.jsx
@@ -11,39 +11,79 @@ function MainForm() {
         text: ''
     })
 
-    const handleChange = name => e => {
-        setValues({ ...values, [name]: e.target.value })
+    const [isValidF, setIsValidF] = useState({
+        firstName: false,
+        lastName: false,
+        middleName: false,
+        telephone: false,
+        text: false
+    })
+
+    const checkValidForm = () => {
+        return Object.values(isValidF).every(field => field === true)
     }
-    
-    const handlerSubmit = async (event) => {
+
+    const validField = (e, len, name) => {
+        if (e.target.value.length >= len) {
+            setIsValidF({ ...isValidF, [name]: true })
+        } else {
+            setIsValidF({ ...isValidF, [name]: false })
+        }
+    }
+
+    const handleChangeTextField = name => e => {
+        setValues({ ...values, [name]: e.target.value })
+        if (!values.text) {
+            validField(e, 3, name)
+        } else {
+            validField(e, 100, name)
+        }
+    }
+
+    const handleChangeNumberField = name => e => {
+        if (!isNaN(parseInt(e.target.value))) {
+            setValues({ ...values, [name]: Number(e.target.value) })
+            validField(e, 10, name)
+        } else {
+            setValues({ ...values, [name]: '' })
+        }
+    }
+
+    const handleSubmit = async (event) => {
         event.preventDefault()
-        const data = JSON.stringify(values)
-        axios.defaults.headers = {'Content-Type': 'application/json'}
-        await axios.post('http://127.0.0.1:8888/processing', data)
+        if (checkValidForm()) {
+            axios.defaults.headers = {'Content-Type': 'application/json'}
+            await axios.post('http://127.0.0.1:8888/processing', values)
+        }
     }
     return(
         <>
-            <Form onSubmit={handlerSubmit} className="form mx-auto">
+            <Form onSubmit={handleSubmit} className="form mx-auto">
                 <h2 className='text-center mb-5'>Форма для обращений граждан</h2>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Group className="mb-3" controlId="formLastName">
                     <Form.Label>Фамилия</Form.Label>
-                    <Form.Control type="text" placeholder="Фамилия" value={values.lastName} onChange={handleChange('lastName')} />
+                    <Form.Control type="text" placeholder="Фамилия" value={values.lastName} onChange={handleChangeTextField('lastName')}
+                    isValid={isValidF.lastName} isInvalid={!isValidF.lastName} />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3" controlId="formFirstName">
                     <Form.Label>Имя</Form.Label>
-                    <Form.Control type="text" placeholder="Имя" value={values.firstName} onChange={handleChange('firstName')} />
+                    <Form.Control type="text" placeholder="Имя" value={values.firstName} onChange={handleChangeTextField('firstName')}
+                    isValid={isValidF.firstName} isInvalid={!isValidF.firstName} />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3" controlId="formMiddleName">
                     <Form.Label>Отчество</Form.Label>
-                    <Form.Control type="text" placeholder="Отчество" value={values.middleName} onChange={handleChange('middleName')} />
+                    <Form.Control type="text" placeholder="Отчество" value={values.middleName} onChange={handleChangeTextField('middleName')}
+                    isValid={isValidF.middleName} isInvalid={!isValidF.middleName} />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3" controlId="formTelephone">
                     <Form.Label>Телефон</Form.Label>
-                    <Form.Control type="tel" placeholder="Телефон" maxLength={10} value={values.telephone} onChange={handleChange('telephone')} />
+                    <Form.Control type="tel" placeholder="Телефон" maxLength={10} value={values.telephone} onChange={handleChangeNumberField('telephone')}
+                    isValid={isValidF.telephone} isInvalid={!isValidF.telephone} />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3" controlId="formText">
                     <Form.Label>Обращение</Form.Label>
-                    <Form.Control as="textarea" placeholder="Обращение" rows={5} value={values.text} onChange={handleChange('text')} />
+                    <Form.Control as="textarea" placeholder="Обращение" rows={5} value={values.text} onChange={handleChangeTextField('text') }
+                    isValid={isValidF.text} isInvalid={!isValidF.text} />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Отправить
